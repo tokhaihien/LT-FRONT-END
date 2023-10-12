c7ab3fb311e1f96961b0b458ab809edc56395dec
tokhaihien <khaihienprivate@gmail.com>
2023-10-12 14:04:58 +0700

    update style.css

--- src/components/header.js
+++ src/components/header.js
@@ -24,14 +24,25 @@ function Header() {
                                         <a href="#"><i class="fa fa-pinterest-p"></i></a>
                                     </div>
 
-                                    <div class="header__top__right__auth me-4">
+                                    <div class="header__top__right__auth mx-3">
                                         <a href="/dang_ky"><i class="fa fa-user"></i> Đăng ký</a>
+                                        
                                     </div>
-                                    <div class="header__top__right__auth me-4">
+                                    <div class="header__top__right__auth mx-3">
                                         <a href="/dang_nhap"><i class="fa fa-user"></i> Đăng nhập</a>
+                                        
                                     </div>
-                                    <div class="header__top__right__auth">
-                                        <a href="/quan_ly_tai_khoan"><i class="fa fa-user"></i> Profile</a>
+
+                                    <div class="header__top__right__language">
+                                    <i class="fa fa-user mx-2"/>
+                                            <div>Tài khoản</div>
+                                            <span class="arrow_carrot-down"></span>
+                                            <ul>
+                                                <li><a href="#">Quản lý tài khoản</a></li>
+                                                <li><a href="#">Quản lý đơn hàng</a></li>
+                                                <li><a href="#">Đổi mật khẩu</a></li>
+                                                <li><a href="#">Đăn xuất</a></li>
+                                            </ul>
                                     </div>
                                     
                                     
